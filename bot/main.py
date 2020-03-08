ba24c7e6542a66873ad03cf7f5d28f64656a38be
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-03-09 01:25:33 +0900

    Create transrate bot

---
+++ bot/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, abort
+from bot import app
+from linebot import (
+        LineBotApi, WebhookHandler
+        )
+from linebot.exceptions import (
+        InvalidSignatureError
+        )
+from linebot.models import (
+        MessageEvent, TextMessage, TextSendMessage
+        )
+import os
+from googletrans import Translator
+
+#環境変数取得
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent)
+def handle_message(event):
+    translator = Translator()
+    message = translator.translate(event.message.text, dest='en').text
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=message)
+            )
+
+if __name__ == "__main__":
+    print("Please run run.py")
